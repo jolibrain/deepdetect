b179f9f6e5e040a1c7abf67789a005801b6ec6ad
Emmanuel Benazera <beniz@droidnik.fr>
2014-11-09 15:05:55 +0100

    working classification with mean image substraction

--- src/caffelib.cc
+++ src/caffelib.cc
@@ -84,37 +84,19 @@ namespace dd
   template <class TInputConnectorStrategy, class TOutputConnectorStrategy, class TMLModel>
   int CaffeLib<TInputConnectorStrategy,TOutputConnectorStrategy,TMLModel>::predict(const APIData &ad)
   {
-    //TODO: get blobs out of input connector.
-    //this->_inputc.transform(); //TODO: instanciate local input connector around ad.
-    /*std::vector<cv::Mat> imgs(1,this->_inputc._image);
-    std::vector<int> labels(1,0);
-    const std::shared_ptr<caffe::ImageDataLayer<float>> image_data_layer
-      = boost::static_pointer_cast<caffe::ImageDataLayer<float>>(_net.layer_by_name("data")); // XXX: std::static_pointer_cast does not work here
-      image_data_layer->AddImagesAndLabels(imgs,labels);*/
     TInputConnectorStrategy inputc;
     inputc.transform(ad);
     Datum datum;
     //ReadImageToDatum(inputc._imgfname,1,227,227,&datum);
-    CVMatToDatum(inputc._image,&datum);
-    //std::cout << "datum height=" << datum.height() << std::endl;
-    //Blob<float> *blob = new Blob<float>(1,datum.channels(),datum.height(),datum.width()); 
-    /*if (!this->_mlmodel._mean.empty())
-      {
-	std::cout << "sub mean\n";
-	std::cout << "input num=" << blob.num() << std::endl;
-	std::cout << "data mean num=" << _data_mean.num() << std::endl;
-	std::cout << "data mean count=" << _data_mean.count() << std::endl;
-	int offset = blob.offset(0);
-	std::cout << "offset=" << offset << std::endl;
-	std::cout << _data_mean.cpu_data() << std::endl;
-	caffe::caffe_sub(_data_mean.count(),blob.mutable_cpu_data()+offset,
-			 _data_mean.cpu_data(),blob.mutable_cpu_data()+offset);
-			 }*/
+    //CVMatToDatum(inputc._image,&datum);
     //std::vector<Blob<float>*> bottom = {blob};
-    //std::cout << blob << std::endl;
     float loss = 0.0;
-    std::vector<Datum> dv = {datum};
-    boost::dynamic_pointer_cast<caffe::MemoryDataLayer<float>>(_net->layers()[0])->AddDatumVector(dv);
+    //std::vector<Datum> dv = {datum};
+    std::vector<cv::Mat> dv = {inputc._image};
+    std::vector<int> dvl = {0};
+    //boost::dynamic_pointer_cast<caffe::MemoryDataLayer<float>>(_net->layers()[0])->AddDatumVector(dv);
+    boost::dynamic_pointer_cast<caffe::MemoryDataLayer<float>>(_net->layers()[0])->AddMatVector(dv,dvl);
+    
     //std::vector<Blob<float>*> results = _net->Forward(bottom,&loss);
     std::vector<Blob<float>*> results = _net->ForwardPrefilled(&loss);
     //std::cout << "accuracy=" << results[1]->cpu_data()[0] << std::endl;
@@ -129,12 +111,7 @@ namespace dd
 	  }
       }
     std::cout << "bcat=" << bcat << " -- accuracy=" << bprob << std::endl;
-    
-    /*int startl = 0;
-    int endl = _net.layers().size()-1;
-    double loss = _net.ForwardFromTo(startl,endl);
-    std::cout << _net.net_output_blobs_.size() << std::endl;*/
-    
+        
     return 0;
   }
 
