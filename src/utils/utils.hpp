2b37865817537e35a55f666e899059e0aa6f8f4f
Emmanuel Benazera <beniz@droidnik.fr>
2015-02-25 19:02:17 +0100

    new utils file

---
+++ src/utils/utils.hpp
@@ -0,0 +1,38 @@
+/**
+ * DeepDetect
+ * Copyright (c) 2014-2015 Emmanuel Benazera
+ * Author: Emmanuel Benazera <beniz@droidnik.fr>
+ *
+ * This file is part of deepdetect.
+ *
+ * deepdetect is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * deepdetect is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with deepdetect.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace dd
+{
+  class dd_utils
+  {
+  public:
+    static std::vector<std::string> split(const std::string &s, char delim)
+    {
+      std::vector<std::string> elems;
+      std::stringstream ss(s);
+      std::string item;
+      while (std::getline(ss, item, delim)) {
+	elems.push_back(item);
+      }
+      return elems;
+    }
+  };
+}
