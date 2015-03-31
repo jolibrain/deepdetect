c5019de619040325110eb940ad13ff1ccce5ee2d
Emmanuel Benazera <beniz@droidnik.fr>
2015-03-31 09:39:23 +0200

    adding code for testing server

---
+++ tests/test-server.cc
@@ -0,0 +1,28 @@
+#include <boost/network/protocol/http/server.hpp>
+#include <string>
+
+namespace http = boost::network::http;
+
+struct handler;
+typedef http::server<handler> http_server;
+
+struct handler {
+  void operator() (http_server::request const &request,
+		   http_server::response &response) {
+    response = http_server::response::stock_reply(
+						  http_server::response::ok, "Hello, world!");
+  }
+
+  void log(http_server::string_type const &info) {
+    std::cerr << "ERROR: " << info << '\n';
+  }
+};
+
+int main(int arg, char * argv[]) {
+  handler handler_;
+  http_server::options options(handler_);
+  http_server server_(
+		      options.address("0.0.0.0")
+		      .port("8000"));
+  server_.run();
+}
