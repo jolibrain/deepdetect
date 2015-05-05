49ad5484334eef124c8dfa8c6f007b60d5b475d7
Emmanuel Benazera <beniz@droidnik.fr>
2015-05-05 18:55:24 +0200

    first set of unit tests for HTTP API server

--- tests/ut-httpapi.cc
+++ tests/ut-httpapi.cc
@@ -22,18 +22,138 @@
 #include "deepdetect.h"
 #include "httpjsonapi.h"
 #include <gtest/gtest.h>
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Easy.hpp>
+#include <curlpp/Options.hpp>
+#include <curlpp/Infos.hpp>
 #include <iostream>
 
 using namespace dd;
 
 std::string host = "127.0.0.1";
-std::string port = "8080";
+int port = 8080;
 int nthreads = 10;
+std::string serv = "myserv";
+std::string serv_put = "{\"mllib\":\"caffe\",\"description\":\"example classification service\",\"type\":\"supervised\",\"parameters\":{\"input\":{\"connector\":\"csv\"},\"mllib\":{\"template\":\"mlp\",\"nclasses\":2,\"layers\":[50,50,50],\"activation\":\"PReLU\"}},\"model\":{\"repository\":\".\"}}";
+
+void get_call(const std::string &url,
+	      const std::string &http_method,
+	      int &outcode,
+	      std::string &outstr)
+{
+  curlpp::Cleanup cl;
+  std::ostringstream os;
+  curlpp::Easy request;
+  curlpp::options::WriteStream ws(&os);
+  curlpp::options::CustomRequest pr(http_method);
+  request.setOpt(curlpp::options::Url(url));
+  request.setOpt(ws);
+  request.setOpt(pr);
+  request.perform();
+  outstr = os.str();
+  std::cout << "outstr=" << outstr << std::endl;
+  outcode = curlpp::infos::ResponseCode::get(request);
+}
+
+void post_call(const std::string &url,
+	       const std::string &jcontent,
+	       const std::string &http_method,
+	       int &outcode,
+	       std::string &outstr)
+{
+  curlpp::Cleanup cl;
+  std::ostringstream os;
+  curlpp::Easy request_put;
+  curlpp::options::WriteStream ws(&os);
+  curlpp::options::CustomRequest pr(http_method);
+  request_put.setOpt(curlpp::options::Url(url));
+  request_put.setOpt(ws);
+  request_put.setOpt(pr);
+  std::list<std::string> header;
+  header.push_back("Content-Type: application/json");
+  request_put.setOpt(curlpp::options::HttpHeader(header));
+  request_put.setOpt(curlpp::options::PostFields(jcontent));
+  request_put.setOpt(curlpp::options::PostFieldSize(jcontent.length()));
+  request_put.perform();
+  outstr = os.str();
+  std::cout << "outstr=" << outstr << std::endl;
+  outcode = curlpp::infos::ResponseCode::get(request_put);
+}
 
 TEST(httpjsonapi,info)
 {
+  ::google::InitGoogleLogging("ut_httpapi");
   HttpJsonAPI hja;
-  hja.start_server_daemon(host,port,nthreads);
-  sleep(3);
+  hja.start_server_daemon(host,std::to_string(port),nthreads);
+  std::string luri = "http://" + host + ":" + std::to_string(port);
+  sleep(2);
+  
+  int code = -1;
+  std::string jstr;
+  get_call(luri+"/info","GET",code,jstr);
+  
+  ASSERT_EQ(200,code);
+  rapidjson::Document d;
+  d.Parse(jstr.c_str());
+  ASSERT_FALSE(d.HasParseError());
+  ASSERT_TRUE(d.HasMember("status"));
+  ASSERT_TRUE(d.HasMember("head"));
+  ASSERT_TRUE(d["head"].HasMember("services"));
+  ASSERT_EQ(0,d["head"]["services"].Size());
+  
+  hja.stop_server();
+}
+
+TEST(httpjsonapi,services)
+{
+  
+  HttpJsonAPI hja;
+  hja.start_server_daemon(host,std::to_string(++port),nthreads);
+  std::string luri = "http://" + host + ":" + std::to_string(port);
+  sleep(2);
+
+  // service creation
+  int code = 1;
+  std::string jstr;
+  post_call(luri+"/services/"+serv,serv_put,"PUT",
+	    code,jstr);  
+  ASSERT_EQ(201,code);
+  rapidjson::Document d;
+  d.Parse(jstr.c_str());
+  ASSERT_FALSE(d.HasParseError());
+  ASSERT_TRUE(d.HasMember("status"));
+  ASSERT_EQ(201,d["status"]["code"].GetInt());
+
+  // service info
+  get_call(luri+"/services/"+serv,"GET",code,jstr);
+  d = rapidjson::Document();
+  d.Parse(jstr.c_str());
+  ASSERT_EQ(200,code);
+  ASSERT_TRUE(d.HasMember("status"));
+  ASSERT_TRUE(d.HasMember("body"));
+  ASSERT_EQ("caffe",d["body"]["mllib"]);
+  ASSERT_EQ("myserv",d["body"]["name"]);
+  
+  // info call
+  get_call(luri+"/info","GET",code,jstr);
+  d = rapidjson::Document();
+  d.Parse(jstr.c_str());
+  ASSERT_EQ(200,code);
+  ASSERT_TRUE(d["head"].HasMember("services"));
+  ASSERT_EQ(1,d["head"]["services"].Size());
+
+  // delete call
+  get_call(luri+"/services/"+serv,"DELETE",code,jstr);
+  d.Parse(jstr.c_str());
+  ASSERT_EQ(200,code);
+
+  // info call
+  get_call(luri+"/info","GET",code,jstr);
+  d = rapidjson::Document();
+  d.Parse(jstr.c_str());
+  ASSERT_EQ(200,code);
+  ASSERT_TRUE(d["head"].HasMember("services"));
+  ASSERT_EQ(0,d["head"]["services"].Size());
+  
   hja.stop_server();
 }
