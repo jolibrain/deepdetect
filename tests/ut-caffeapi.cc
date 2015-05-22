49968b41588749c34984426bc81447353bdb41fe
Emmanuel Benazera <beniz@droidnik.fr>
2015-05-22 11:09:39 +0200

    fixed typo in caffeapi unit tests

--- tests/ut-caffeapi.cc
+++ tests/ut-caffeapi.cc
@@ -40,7 +40,7 @@ static std::string model_templates_repo = "../templates/caffe/";
 
 #ifndef CPU_ONLY
 static std::string iterations_mnist = "100";
-static std::string interations_plank = "2000";
+static std::string iterations_plank = "2000";
 static std::string iterations_forest = "2000";
 #else
 static std::string iterations_mnist = "50";
