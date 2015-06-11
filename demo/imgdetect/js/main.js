f06935fdfe764c8199ca774562842abe5550c8b0
Alexandre Girard <git@alexgirard.com>
2015-06-11 16:51:11 +0200

    working imgdetect demo, for dev only with custom nginx rules

--- demo/imgdetect/js/main.js
+++ demo/imgdetect/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
     $('.loading').removeClass('hidden');
 
-    var post_url = "http://" + $('#server_ip').val() + ":" + $('#server_port').val() + "/predict";
+    var post_url = "/api/predict";
     var post_data = {
       service: "imageserv",
       parameters: {
@@ -14,44 +14,50 @@ $(document).ready(function() {
       data: [$('input#url').val()]
     };
 
-    $.post(post_url, post_data)
-    .done(function(data) {
-      data = JSON.parse(data);
-      $('#emptyImage img').attr('src', post_data.data[0]);
-      $('#emptyImage ul').html('');
-      $.each(data.predictions.classes, function() {
-        var percent = parseInt(this.prob * 100);
+    console.log(JSON.stringify(post_data));
 
-        var style = 'success';
+    $.ajax({
+      type: "POST",
+      url: post_url,
+      data: JSON.stringify(post_data),
+      dataType: 'json',
+      success: function(data) {
+        $('#emptyImage img').attr('src', post_data.data[0]);
+        $('#emptyImage ul').html('');
+        $.each(data.body.predictions.classes, function() {
+          var percent = parseInt(this.prob * 100);
 
-        if(percent < 60) {
-          style='warning';
-        }
+          var style = 'success';
 
-        if(percent < 25) {
-          style = 'danger';
-        }
+          if(percent < 60) {
+            style='warning';
+          }
 
-        var re = /(n\d+) (.*)/;
-        var md = this.cat.match(re);
-        if(md.size == 3) {
-          var nid = md[1];
-          var name = md[2];
+          if(percent < 25) {
+            style = 'danger';
+          }
 
-          var predictionHtml = '<div class="row"><div class="col-lg-4"><div class="progress">';
-          predictionHtml += '<div class="progress-bar progress-bar-' + style + '" role="progressbar" ';
-          predictionHtml += 'aria-valuenow="' + percent + '" aria-valuemin="0" aria-valuemax="100" ';
-          predictionHtml += 'style="width: ' + percent + '%;">' + percent + '%</div></div></div>';
-          predictionHtml += '<div class="col-lg-8"><a target="_blank" href="http://www.image-net.org/synset?wnid=' + nid + '">' + name + '</a></div></div>';
-          $('#emptyImage .predictions').append(predictionHtml);
-        }
-      });
+          var re = /(n\d+) (.*)/;
+          var md = this.cat.match(re);
+          if(md.length == 3) {
+            var nid = md[1];
+            var name = md[2];
 
-      $('#imageList').prepend('<hr>');
-      $('#imageList').prepend($('#emptyImage').clone().attr('id', '').removeClass('hidden'));
-      $('.loading').addClass('hidden');
+            var predictionHtml = '<div class="row"><div class="col-lg-4"><div class="progress">';
+            predictionHtml += '<div class="progress-bar progress-bar-' + style + '" role="progressbar" ';
+            predictionHtml += 'aria-valuenow="' + percent + '" aria-valuemin="0" aria-valuemax="100" ';
+            predictionHtml += 'style="width: ' + percent + '%;">' + percent + '%</div></div></div>';
+            predictionHtml += '<div class="col-lg-8"><a target="_blank" href="http://www.image-net.org/synset?wnid=' + nid + '">' + name + '</a></div></div>';
+            $('#emptyImage .predictions').append(predictionHtml);
+          }
+        });
 
-      $('input#url').val('');
+        $('#imageList').prepend('<hr>');
+        $('#imageList').prepend($('#emptyImage').clone().attr('id', '').removeClass('hidden'));
+        $('.loading').addClass('hidden');
+
+        $('input#url').val('');
+      }
     });
 
   });
