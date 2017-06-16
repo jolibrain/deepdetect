4eff2c804d5a93bbdc7c0874855c9e3ff5955598
Jakkapong Saksrisuwan <jsaksris@mtu.edu>
2017-06-16 11:28:24 +0200

    don't care

--- githash.h
+++ githash.h
@@ -2,6 +2,6 @@
 #define GITHASH_H
 
 #define GIT_BRANCH "travis"
-#define GIT_COMMIT_HASH "9160b791a4bbbd47d811bddd8a48bb9e0de77b65"
+#define GIT_COMMIT_HASH "499d7d17c84c86071ed5bcd04a939c62db935a18"
 
 #endif
