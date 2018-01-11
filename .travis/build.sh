d116466a02a9936773b140e6d4936d9e2599498a
Emmanuel Benazera <beniz@droidnik.fr>
2018-01-11 15:44:08 +0100

    fixed quotes around cmake CUDA travis build options

--- .travis/build.sh
+++ .travis/build.sh
@@ -14,7 +14,7 @@ CMAKE_ARGS=('-DCMAKE_VERBOSE_MAKEFILE=ON')
 CMAKE_ARGS+=('-DCMAKE_INSTALL_PREFIX=../install')
 if [ "$BUILD_CUDA" = 'true' ]; then
     CMAKE_ARGS+=('-DUSE_CUDNN=ON')
-    CMAKE_ARGS+=('-DCUDA_ARCH="-gencode arch=compute_61,code=sm_61"')
+    CMAKE_ARGS+=('-DCUDA_ARCH=\"-gencode arch=compute_61,code=sm_61\"')
     CMAKE_ARGS+=('-DCUDA_NVCC_EXECUTABLE=/usr/local/bin/nvcc')
     export PATH="/usr/local/cuda/bin:${PATH}"
 else
