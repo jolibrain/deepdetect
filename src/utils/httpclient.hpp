e047258e968cabe7d9159676c10171c8b7ef5106
David Geldreich <david.geldreich@free.fr>
2019-04-30 18:43:46 +0200

    Empty httpclient class on Windows

--- src/utils/httpclient.hpp
+++ src/utils/httpclient.hpp
@@ -19,10 +19,12 @@
  * along with deepdetect.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#ifndef WIN32
 #include <curlpp/cURLpp.hpp>
 #include <curlpp/Easy.hpp>
 #include <curlpp/Options.hpp>
 #include <curlpp/Infos.hpp>
+#endif
 
 #ifndef DD_HTTPCLIENT_H
 #define DD_HTTPCLIENT_H
@@ -38,6 +40,7 @@ namespace dd
 			 int &outcode,
 			 std::string &outstr)
     {
+#ifndef WIN32
       curlpp::Cleanup cl;
       std::ostringstream os;
       curlpp::Easy request;
@@ -51,6 +54,7 @@ namespace dd
       outstr = os.str();
       //std::cout << "outstr=" << outstr << std::endl;
       outcode = curlpp::infos::ResponseCode::get(request);
+#endif
     }
     
     static void post_call(const std::string &url,
@@ -60,6 +64,7 @@ namespace dd
 			  std::string &outstr,
 			  const std::string &content_type="Content-Type: application/json")
     {
+#ifndef WIN32
       curlpp::Cleanup cl;
       std::ostringstream os;
       curlpp::Easy request_put;
@@ -77,6 +82,7 @@ namespace dd
       outstr = os.str();
       //std::cout << "outstr=" << outstr << std::endl;
       outcode = curlpp::infos::ResponseCode::get(request_put);
+#endif
     }
     
   };
