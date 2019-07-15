5e63ef55b0d85c67d15acf640fdaece16d54e1cc
Emmanuel Benazera <emmanuel.benazera@jolibrain.com>
2019-07-15 11:04:36 +0200

    reverting NCNN changes as it appears to break ssd, pinning on commit

--- src/backends/ncnn/ncnnlib.cc
+++ src/backends/ncnn/ncnnlib.cc
@@ -96,7 +96,7 @@ namespace dd
         opt.num_threads = _threads;
         opt.blob_allocator = &_blob_pool_allocator;
         opt.workspace_allocator = &_workspace_pool_allocator;
-	_net->opt = opt;
+        ncnn::set_default_option(opt);
         model_type(this->_mlmodel._params,this->_mltype);
     }
 
