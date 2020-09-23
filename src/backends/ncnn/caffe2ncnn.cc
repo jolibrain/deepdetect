dcf818c6fbe53a2fc5c2dd44264fc4d03fe69009
Mehdi Abaakouk <sileht@sileht.net>
2020-09-23 11:02:19 +0200

    chore(build): protobuf::io::CodedInputStream::SetTotalBytesLimit deprecation
    second parameter is ignored and deprecated.

--- src/backends/ncnn/caffe2ncnn.cc
+++ src/backends/ncnn/caffe2ncnn.cc
@@ -337,7 +337,7 @@ static bool read_proto_from_binary(const char *filepath,
   google::protobuf::io::IstreamInputStream input(&fs);
   google::protobuf::io::CodedInputStream codedstr(&input);
 
-  codedstr.SetTotalBytesLimit(INT_MAX, INT_MAX / 2);
+  codedstr.SetTotalBytesLimit(INT_MAX);
 
   bool success = message->ParseFromCodedStream(&codedstr);
 
