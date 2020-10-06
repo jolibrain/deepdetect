6d36b441415ffed6a689070cc0e4d5ce56f7dc56
Mehdi Abaakouk <sileht@sileht.net>
2020-10-06 11:48:21 +0200

    feat(ci): build docker images for git tags

--- tools/build-docker-images.sh
+++ tools/build-docker-images.sh
@@ -4,26 +4,36 @@ export DOCKER_BUILDKIT=1
 
 TARGETS="cpu/default gpu/default gpu/tf gpu/tensorrt"
 
-if [ "$GIT_BRANCH" == "master" ]; then
+if [ "$TAG_NAME" ]; then
+    TMP_TAG=${TAG_NAME#v}
+elif [ "$GIT_BRANCH" == "master" ]; then
     TMP_TAG="ci"
 else
     TMP_TAG="trash"
 fi
 
-image_url_prefix="ceres:5000/jolibrain/deepdetect"
+image_url_prefix_release="jolibrain/deepdetect"
+image_url_prefix_ci="ceres:5000"
 images_to_push=
 
 for target in $TARGETS ; do
     arch=${target%%/*}
     build=${target##*/}
-    image_url="${image_url_prefix}_${arch}"
-    [ "$build" != "default" ] && image_url="${image_url}_build"
+    image_url_release="${image_url_prefix_release}_${arch}"
+    [ "$build" != "default" ] && image_url_release="${image_url}_build"
+
+    image_url_ci="${image_url_prefix_ci}/${image_url_prefix_release}"
 
     docker build \
-        -t $image_url:$TMP_TAG \
+        -t $image_url_ci:$TMP_TAG \
         --build-arg DEEPDETECT_BUILD=$build \
         -f docker/${arch}.Dockerfile \
         .
+
+    if [ "$TAG_NAME" ]; then
+        docker tag $image_url_ci:$TMP_TAG $image_url_release:$TMP_TAG
+        docker tag $image_url_ci:$TMP_TAG $image_url_release:latest
+    fi
 
     images_to_push="${images_to_push} $image_url:$TMP_TAG"
 done
