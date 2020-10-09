5e9909302e37e7b1ba6fc600d4e64050c1be2bff
Mehdi Abaakouk <sileht@sileht.net>
2020-10-09 15:27:46 +0200

    chore: add docker image to release note

--- tools/release.sh
+++ tools/release.sh
@@ -6,5 +6,15 @@ yarn run standard-version -r $kind
 tag=$(cat package.json | jq -r .version)
 
 sed -ne "/^## $tag/,/^##.*202/p" CHANGELOG.md | sed -e '$d' -e '1d' > note.md
+
+cat >> note.md <<EOF
+### Docker images:
+
+* CPU version: `docker pull jolibrain/deepdetect_cpu:$tag`
+* GPU (CUDA only): `docker pull jolibrain/deepdetect_gpu:$tag`
+* GPU (CUDA and Tensorrt) :`docker pull jolibrain/deepdetect_cpu_tensorrt:$tag`
+* All images available on https://hub.docker.com/u/jolibrain
+EOF
+
 trap "rm -f note.md" EXIT
 gh release create --title "DeepDetect v$tag" -F note.md -d v$tag
