a7327d0022c27c30d2527e34a685cf039068dae2
Mehdi Abaakouk <sileht@sileht.net>
2020-11-12 09:56:40 +0100

    docker: don't install headers on gpu runtime image

--- docker/sync_gpu_dockerfiles.sh
+++ docker/sync_gpu_dockerfiles.sh
@@ -2,23 +2,29 @@
 
 here=$(dirname $(readlink -f $0))
 
-declare -A images
-images[gpu]="nvidia/cuda:10.2-cudnn7-devel-ubuntu18.04"
+declare -A runtime_images
+runtime_images[gpu]="nvidia/cuda:10.2-cudnn7-runtime-ubuntu18.04"
 # Ubuntu 18.04+cuda 10.2.80 + cuDNN 7.6.5 + TensorRT 7.0.0
 # https://docs.nvidia.com/deeplearning/tensorrt/container-release-notes/rel_20-03.html#rel_20-03
-images[gpu_tensorrt]="nvcr.io/nvidia/tensorrt:20.03-py3"
+runtime_images[gpu_tensorrt]="nvcr.io/nvidia/tensorrt:20.03-py3"
+
+declare -A devel_images
+devel_images[gpu]="nvidia/cuda:10.2-cudnn7-devel-ubuntu18.04"
+devel_images[gpu_tensorrt]="nvcr.io/nvidia/tensorrt:20.03-py3"
 
 declare -A builds
 builds[gpu_tensorrt]="tensorrt"
 
-for dest in "${!images[@]}" ; do
-    image=${images[$dest]}
+for dest in "${!runtime_images[@]}" ; do
+    runtime_image=${runtime_images[$dest]}
+    devel_image=${devel_images[$dest]}
     build=${builds[$dest]}
 
     [ ! "$build" ] && build="default"
 
     sed \
-        -e "s,FROM [^ ]*,FROM ${image},g" \
+        -e "s,FROM [^ ]* AS build,FROM ${devel_image} AS build,g" \
+        -e "s,FROM [^ ]* AS runtime,FROM ${runtime_image} AS runtime,g" \
         -e "s,ARG DEEPDETECT_ARCH=.*,ARG DEEPDETECT_ARCH=gpu,g" \
         -e "s,ARG DEEPDETECT_BUILD=.*,ARG DEEPDETECT_BUILD=${build},g" \
         -e "s/\(apt_\(cache\|lib\)\)_cpu/\1_${dest}/g" \
