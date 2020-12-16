ccfe6b588f35adce74937a18f27597cff5001e9c
Emmanuel Benazera <emmanuel.benazera@jolibrain.com>
2020-12-16 17:13:23 +0100

    fix: allows mean+std image transform with NCNN inference

--- src/backends/ncnn/ncnninputconns.h
+++ src/backends/ncnn/ncnninputconns.h
@@ -103,7 +103,16 @@ namespace dd
       _in = ncnn::Mat::from_pixels(bgr.data, ncnn::Mat::PIXEL_BGR, bgr.cols,
                                    bgr.rows);
       if (_has_mean_scalar)
-        _in.substract_mean_normalize(&_mean[0], 0);
+        {
+          if (_std.empty())
+            {
+              _in.substract_mean_normalize(_mean.data(), 0);
+            }
+          else
+            {
+              _in.substract_mean_normalize(_mean.data(), _std.data());
+            }
+        }
       _ids.push_back(this->_uris.at(0));
     }
 
