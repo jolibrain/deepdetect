15d11b736d1bcf0e6e9f098797485a378c3b1b3e
Guillaume Infantes <guillaume.infantes@jolibrain.com>
2021-01-28 13:55:05 +0100

    fix(chore): typo when trying to use syslog

--- src/dd_spdlog.h
+++ src/dd_spdlog.h
@@ -7,7 +7,7 @@
 
 #if SPDLOG_VER_MAJOR > 0
 #ifdef USE_DD_SYSLOG
-#include <spdlog/sinks/syslog_sinks.h>
+#include <spdlog/sinks/syslog_sink.h>
 #define DD_SPDLOG_LOGGER spdlog::syslog_logger_mt
 #else
 #include <spdlog/sinks/stdout_sinks.h>
