2e02543b86af33e8aedab1783100a0193ed3980f
Louis Jean <louis.jean@jolibrain.com>
2023-11-29 15:03:54 +0000

    chore: make torch default in dockers
    remove gpu_torch and cpu_torch dockers

--- ci/build-docker-images.sh
+++ ci/build-docker-images.sh
@@ -14,10 +14,8 @@ NAMES="$@"
 
 declare -A TARGETS
 TARGETS[cpu]="cpu/default"
-TARGETS[cpu_torch]="cpu/torch"
 TARGETS[gpu]="gpu/default"
 TARGETS[gpu_tf]="gpu/tf"
-TARGETS[gpu_torch]="gpu/torch"
 TARGETS[gpu_tensorrt]="gpu_tensorrt/tensorrt"
 
 PR_NUMBER=$(echo $GIT_BRANCH | sed -n '/^PR-/s/PR-//gp')
